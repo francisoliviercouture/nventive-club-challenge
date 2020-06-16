0dfe6ec0fd8a6ca77ca75dee29e5c964251f5cb5
Francis-Olivier Couture <francis-olivier_couture@hotmail.com>
2020-06-16 15:42:11 -0400

    new data

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ import AthleteList from './AthleteList';
 import SectionRandom from './SectionRandom';
 
 function App() {
-  const { athletes, last_updated } = data;
+  const { athletes, date_from, date_to, last_updated } = data;
 
   return (
     <div className="app container">
       <h1>nventive Strava Challenge</h1>
       <h5>Last updated: {last_updated && <TimeAgo datetime={last_updated} locale='fr_CA' />}</h5>
+      <span><b>{date_from} to {date_to}</b></span>
       <AthleteList athletes={athletes} />
-      <SectionRandom athletes={athletes}/>
+      <SectionRandom athletes={athletes} />
     </div>
   );
 }
