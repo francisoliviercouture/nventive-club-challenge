80bb13adf770cef2f1f5f902e853b74db8544c77
Francis-Olivier Couture <francis-olivier_couture@hotmail.com>
2020-06-18 15:27:13 -0400

    meta tags

--- src/components/TableAthlete/SortedListAthlete/index.tsx
+++ src/components/TableAthlete/SortedListAthlete/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent} from 'react';
+import React, { FunctionComponent } from 'react';
 import { Athlete } from '../../../models/athlete.model';
 
 interface SortedListAthleteProps {
