c2741a28a60982069980593902034086bf8e1a4b
Francis-Olivier Couture <francis-olivier_couture@hotmail.com>
2020-07-06 08:48:34 -0400

    new data

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/strava-club-challenge/precache-manifest.c0c97b3001612b1e372835265e680273.js"
+  "/strava-club-challenge/precache-manifest.745645abcff89d184b2f9e40ac157585.js"
 );
 
 self.addEventListener('message', (event) => {
