8eb77efa5ff5260bc4c69dfd1df870452f72f1a9
Francis-Olivier Couture <francis-olivier_couture@hotmail.com>
2020-07-06 08:49:13 -0400

    Updates

--- precache-manifest.f6d1bfa639a847fe2be87d1ba5aa1584.js
+++ precache-manifest.f6d1bfa639a847fe2be87d1ba5aa1584.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c83c4d0eeb7e291aacfbdf7123c20fc",
+    "revision": "f7899bb7ebc14818c362c681b20f1377",
     "url": "/strava-club-challenge/index.html"
   },
   {
-    "revision": "e3b7fdd96fb21d4a7688",
+    "revision": "edba680d7ed6d5481d7f",
     "url": "/strava-club-challenge/static/css/main.2fe2330f.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/strava-club-challenge/static/js/2.c8d95e4e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e3b7fdd96fb21d4a7688",
-    "url": "/strava-club-challenge/static/js/main.2e303b2b.chunk.js"
+    "revision": "edba680d7ed6d5481d7f",
+    "url": "/strava-club-challenge/static/js/main.bb346538.chunk.js"
   },
   {
     "revision": "b8ca6a077b70f77fa030",
